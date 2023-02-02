1b7c8b415aed85c1e2592a4a1c79084dbb58f5a8
Hélène Sauvé <helenesauve@Helenes-MBP.lan>
2023-02-02 19:05:10 +0000

    getting a response from geocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,25 +6,24 @@ var geocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + 
 
 
 
+
 // var queryUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=" + myKey
 
 $.ajax({
   url: geocodeUrl,
   method: "GET"
 }).then (queryCoffeesFromGeocodeResponse); {
-  // call queryCoffeesFromGeocodeResponse
 
-  // create variable for latitude and longitude
   // other ajax call
 };
 
 function queryCoffeesFromGeocodeResponse (response) {
   
-  var latitude = response.results.location.lat;
-  var longitude = response.results.location.lng;
+  var latitude = response.results[0].geometry.location.lat;
+  var longitude = response.results[0].geometry.location.lng;
 
   // build queryurl
-  var queryUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&types=food&key=" + myKey
+  var queryUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&radius=500&types=food" + "&key=" + myKey
   console.log(queryUrl)
   
   // call ajax with queryurl
