a724b26f25f4acc4d6c9c8d1b5d18374129b1fca
Hélène Sauvé <helenesauve@Helenes-MBP.lan>
2023-02-02 20:44:25 +0000

    Added a function that uses a for loop to iterate through JSON api response and dynamically add text to HTML'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,36 @@
-// Pseudocode here
-var city = "Bristol"
-// var queryUrl = "https://www.google.com/maps/embed/v1/MAP_MODE?key="+ myKey + "&city=" + city;
-
-var geocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=" + myKey
-
-
-
-
-// var queryUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=" + myKey
-
+// when button is clicked, the function starts
+$("button").on("click", function() {
+  console.log(city)
+  var city = $("#input-selector").val()
+  var geocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=" + myKey
 $.ajax({
   url: geocodeUrl,
   method: "GET"
-}).then (queryCoffeesFromGeocodeResponse); {
-
-  // other ajax call
-};
-
+}).then (queryCoffeesFromGeocodeResponse);
+});
+// function that query coffees in the location searched
 function queryCoffeesFromGeocodeResponse (response) {
-  
   var latitude = response.results[0].geometry.location.lat;
   var longitude = response.results[0].geometry.location.lng;
-
   // build queryurl
-  var queryUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&radius=500&types=food" + "&key=" + myKey
+  var queryUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&radius=500&types=cafe" + "&key=" + myKey
   console.log(queryUrl)
-  
-  // call ajax with queryurl
-}
-
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  }).then(function(response) {
+    // loop through the results in the response data
+    for (var i = 0; i < response.results.length; i++) {
+      // get the coffee name and rating
+      var coffeeName = response.results[i].name;
+      var rating = response.results[i].rating;
+      console.log(coffeeName);
+      console.log(rating);
+      // add to html
+      // add the coffee name and rating to the div
+      $('#shop1').text("Coffee shop: " + coffeeName + "Rating: " +  rating);
+      // append the div to the container
+    }
+  }
+)
+};
